2dc45f06f1853a53e21ff5d3fec392001f0d1239
r4bb1t <r4bb1t.github@gmail.com>
2024-02-11 12:38:25 +0900

    feat: add logo

--- app/components/layout/header/index.tsx
+++ app/components/layout/header/index.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import AddPlaylist from "./add-playlist";
 import SignIn from "./sign-in";
+import Logo from "@/app/logo.svg";
 
 export default function Header() {
   return (
     <div className="navbar fixed inset-x-0 top-0 z-20 h-16 justify-between bg-base-100">
       <Link href={"/"} className="btn btn-ghost gap-0 text-xl">
-        learnstream
+        <Logo className="h-6 fill-white" />
       </Link>
       <SignIn />
     </div>
