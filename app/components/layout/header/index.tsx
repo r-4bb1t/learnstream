31f294359ee0277ca5d942406996b6a96585e533
r4bb1t <r4bb1t.github@gmail.com>
2024-02-11 12:24:13 +0900

    chore: remove 'academy' in header

--- app/components/layout/header/index.tsx
+++ app/components/layout/header/index.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className="navbar fixed inset-x-0 top-0 z-20 h-16 justify-between bg-base-100">
       <Link href={"/"} className="btn btn-ghost gap-0 text-xl">
-        learnstream<sup>academy</sup>
+        learnstream
       </Link>
       <SignIn />
     </div>
