b6c61b93fb8dea2fb1b8d9a184d48c574d7eb838
r4bb1t <r4bb1t.github@gmail.com>
2024-02-13 00:34:08 +0900

    style: card style

--- app/main/card.tsx
+++ app/main/card.tsx
@@ -55,11 +55,11 @@ export default function MainCard({
             addSuffix: true,
           })}
         </span>
-        <div className="relative mt-1 flex h-8 gap-2 overflow-clip opacity-80 hover:flex-wrap hover:overflow-visible [&:hover>svg]:hidden">
+        <div className="relative mt-1.5 flex h-8 gap-2 overflow-clip opacity-80 hover:flex-wrap hover:overflow-visible [&:hover>svg]:hidden">
           {playlist.tags.map((tag) => (
             <div
               key={tag}
-              className="h-fit whitespace-nowrap rounded-md border border-primary bg-base-100 px-1 py-0.5 text-xs capitalize text-primary"
+              className="h-fit whitespace-nowrap rounded-md border border-primary bg-base-100 px-1 py-px text-xs capitalize text-primary"
             >
               {tag}
             </div>
